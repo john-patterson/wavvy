61ab62e71811ee15c161131308aa63639622dd34
John Patterson <john@JohnPPatterson.com>
2016-12-31 12:57:28 -0600

    Smoothed out the login logout mechanism

--- wavvy.py
+++ wavvy.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, render_template, request, session
+from flask import Flask, url_for, render_template, request, session, escape
 
 app = Flask(__name__)
 
@@ -9,10 +9,28 @@ def hello(name=None):
     return render_template('hello.html', name=name)
 
 
+@app.route('/')
+def index():
+    if session.get('logged_in', False):
+        return 'Logged in as {}'.format(escape(session['username']))
+    return 'You are not logged in.'
+
+
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     error = None
     if request.method == 'POST':
-        session.user_logged_in = True
-        return 'Validating a login! U:{} P:{}'.format(request.form['username'], request.form['password'])
+        session['logged_in'] = True
+        session['username'] = request.form['username']
+        password = escape(request.form['password'])
+        return 'Validating a login! U:{} P:{}'.format(escape(session['username']), password)
     return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    try:
+        session.logged_in = False
+    except AttributeError:
+        session.logged_in = False
+    return 'You are logged out.'
