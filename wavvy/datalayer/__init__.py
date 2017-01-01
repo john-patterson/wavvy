6293f8238b51f8e79a6818434423f0cafa90494d
John Patterson <john@JohnPPatterson.com>
2017-01-01 17:48:35 -0600

    Added `adjust` to datalayer __init__

--- wavvy/datalayer/__init__.py
+++ wavvy/datalayer/__init__.py
@@ -1,4 +1,4 @@
 from wavvy.datalayer.models import User, Adjustment
-from wavvy.datalayer.procedures import add_user, authenticate
+from wavvy.datalayer.procedures import add_user, authenticate, adjust
 from wavvy.datalayer.wave import Thermostat
 from wavvy.datalayer.weather import OpenWeather
