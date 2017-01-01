fd987e207ccb685ba72c6bef20789e8b273590c1
John Patterson <john@JohnPPatterson.com>
2017-01-01 17:12:22 -0600

    Disabled modification tracking
    This improves performance and shuts up SQLAlchemy

--- wavvy/__init__.py
+++ wavvy/__init__.py
@@ -5,6 +5,7 @@ from flask import Flask
 app = Flask(__name__)
 app.secret_key = os.environ['WAVVY_SECRET_KEY']
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 import wavvy.views
 import wavvy.datalayer
