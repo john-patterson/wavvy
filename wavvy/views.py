c263d92b7655212156f419a959c5ca99adc713c9
John Patterson <john@JohnPPatterson.com>
2017-01-01 23:35:13 -0600

    Fixed redirect to adjust
    I forgot the wraps decorator on my requires_auth function, so that's why
    werkzeug could not resolve the url_for on adjust.
    
    Also move the current_temp and current_setting assignment to after the
    POST block so that the redirect gets the refreshed data.

--- wavvy/views.py
+++ wavvy/views.py
@@ -2,9 +2,11 @@ from flask import render_template, request, session, escape, redirect, url_for
 
 from wavvy import app, user
 import wavvy.data as data
+from functools import wraps
 
 
 def requires_auth(view):
+    @wraps(view)
     def inner(*args, **kwargs):
         if user.logged_in():
             return view(*args, **kwargs)
@@ -23,11 +25,11 @@ def index():
 @app.route('/adjust', methods=['POST', 'GET'])
 @requires_auth
 def adjust():
-    current_temp = data.current_room_temp()
-    current_setting = data.get_current_setting()
     if request.method == 'POST':
         new_temp = request.form['new_temp']
         data.adjust_temp(new_temp)
+    current_temp = data.current_room_temp()
+    current_setting = data.get_current_setting()
     return render_template('adjust.html',
                            error=None,
                            current_temp=current_temp,
